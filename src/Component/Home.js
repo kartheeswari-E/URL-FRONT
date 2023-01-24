4253469ea91dccee9b32d70e458bb50eced5973c
kartheeswari <kartheesw2001@gmail.com>
2023-01-25 00:37:37 +0530

    api

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -7,7 +7,7 @@ function Home() {
     const data = {
    LongUrl:url
     };
-  fetch(`${process.env.REACT_APP_BASE_URL}/api/longurl`, {
+  fetch(`${process.env.REACT_APP_BASE_URL}/longurl`, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
@@ -27,7 +27,7 @@ function Home() {
     
 const [part, setpart] = useState([]);
 useEffect(() => {
-  fetch(`${process.env.REACT_APP_BASE_URL}/api/shorturl`)
+  fetch(`${process.env.REACT_APP_BASE_URL}/shorturl`)
     .then((data) => data.json())
     .then((parts) => setpart(parts));          
 },[]);
