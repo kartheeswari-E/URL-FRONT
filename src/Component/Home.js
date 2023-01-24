224344a361285dbec56eec6efbf4c40fe3b3bbee
kartheeswari <kartheesw2001@gmail.com>
2023-01-25 00:06:05 +0530

    commit one

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -7,7 +7,7 @@ function Home() {
     const data = {
    LongUrl:url
     };
-  fetch(`${process.env.URLS}/longurl`, {
+  fetch(`${process.env.URLS}/api/longurl`, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
@@ -27,7 +27,7 @@ function Home() {
     
 const [part, setpart] = useState([]);
 useEffect(() => {
-  fetch(`${process.env.URLS}/shorturl`)
+  fetch(`${process.env.URLS}/api/shorturl`)
     .then((data) => data.json())
     .then((parts) => setpart(parts));          
 },[]);
